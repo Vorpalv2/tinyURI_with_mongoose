fa75a253f380d82e38abcd398a476cd79c3c8b19
Aakash <aakashanand1995@gmail.com>
2024-02-27 19:01:12 +0530

    added push functionality

--- backend/routes/url.route.js
+++ backend/routes/url.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { shortGenerator } from "../controller/shorturlgenerator.controller.js";
+import { url } from "../model/url.model.js";
 
 const urlroute = Router();
 
@@ -8,5 +9,21 @@ urlroute.get(`/`, (req, res) => {
 });
 
 urlroute.post(`/`, shortGenerator);
+
+urlroute.get(`/:shortid`, async (req, res) => {
+  let shortid = req.params.shortid;
+  console.log(shortid);
+  let foundData = await url.findOneAndUpdate(
+    { tinyurl: shortid },
+    {
+      $push: {
+        visitHistory: { timestamp: Date.now() },
+      },
+    }
+  );
+  console.log(foundData);
+
+  res.redirect(foundData?.redirecturl);
+});
 
 export { urlroute };
