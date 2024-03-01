a4cbb2ac9945b35a8620220b30a0e27730768bea
Aakash <aakashanand1995@gmail.com>
2024-03-01 12:00:07 +0530

    added users collection and route update

--- backend/routes/static.route.js
+++ backend/routes/static.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { url } from "../model/url.model.js";
+import { user } from "../model/user.model.js";
 
 const staticRouter = Router();
 
@@ -7,6 +8,20 @@ staticRouter.get(`/`, async (req, res) => {
   const result = await url.find({});
 
   res.render("homepage", { data: result });
+});
+
+staticRouter.get(`/register`, (req, res) => {
+  res.render("register");
+});
+
+staticRouter.post(`/register`, async (req, res) => {
+  const { username, password, email } = req.body;
+  const createdUser = await user.create({
+    username,
+    password,
+    email,
+  });
+  res.status(200).send(createdUser);
 });
 
 export { staticRouter };
